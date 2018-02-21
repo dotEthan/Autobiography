a57ab4e89f355bb0a4feb9901881ace8eaa0d594
Ethan Strauss <estrauss@gmail.com>
2018-02-20 19:40:05 -0500

    Update script.js
    Colour picking is working

--- mastermind/script.js
+++ mastermind/script.js
@@ -1,28 +1,28 @@
 let winnerColors = [];
 let currentColors;
+let choiceColor;
 
 let colors = [
   "#ee799f", //pink
-  "#da70d6", //purple
+  "#660066", //purple
   "#000080", //dark blue
   "#48d1cc", //light blue
-  "#A2B5CD", //grey
   "#006400", //forest green
   "#caff70", //bright green
-  "#8b2323", //dark red
-  "#ff4040", //bright red
-  "#ff7f24", //orange
+  "#800000", //dark red
+  "#ff0000", //bright red
+  "#663300", //brown
   "#ffff00", //yellow
-] 
+]
 
-function pickColors() {
-  for (let i = 0; i<4; i++){
-    winnerColors.push(Math.floor(Math.random()*10));
-  }
+let choices = document.querySelectorAll('.choicebutton')
+for (let i=0;i<choices.length;i++){
+  choices[i].addEventListener('click', chooseColor);
 }
-pickColors();
-console.log(winnerColors);
 
-function reset() {
+function chooseColor(e) {
+  if( !e ) e = window.event;
+  let chosenColor = document.getElementById('colorchoice');
+  chosenColor.style.backgroundColor = colors[e.target.innerText];
 }
 
