bccf50810e78744f51c30a368eb6a8c1c797be05
Ethan Strauss <estrauss@gmail.com>
2018-03-23 16:09:48 -0400

    Update script.js
    Two days. working on footericons

--- script.js
+++ script.js
@@ -1,42 +1,71 @@
-let projectWidth = $(".project").width();
-let projectHeight = $(".project").height();
-console.log(projectHeight);
-// $('.bottom').left = projectWidth;
-// $('.bottom').top = projectHeight;
+//------------------------
+// Project Icons hovering
+//------------------------
+let projectWidth = $(".project").width().toString();
+let projectHeight = $(".project").height().toString();
+let bottoms = $('.bottom');
+let tops = $('.top');
+$('.bottom')  
+  .css({ borderBottomWidth: projectHeight + 'px', borderLeftWidth: projectWidth + 'px' })
+  .addClass('animate');
+$('.top')  
+  .css({ borderRightWidth: projectWidth + 'px', borderTopWidth: projectHeight + 'px' })  
+  .addClass('animate');
+
 $('.project').hover(function() {
   let divChilds = jQuery("div", this);
-  divChilds.each(function(x, elem) {
-    if(elem.className == "top"){
-      elem.style.top = '0px';
-      elem.style.left = '0px';
-      console.log(elem);
-    } else if(elem.className == "bottom"){
-      elem.style.top = '0px';
-      elem.style.left = '0px';
-    } else if(elem.className == "protext"){
-      elem.style.fontSize = '1.7em';
-      elem.style.marginTop = '10px';
-      elem.style.marginbottom = '10px';
-      elem.style.backgroundColor = 'rgba(255, 218, 218, 0.8)';
-    } else if(elem.className == "protextw"){
-      elem.style.fontSize = '0.7em';
+  divChilds.each(function() {
+    // console.log(this);
+    if(this.className == "top animate"){
+      this.style.top = '0px';
+      this.style.left = '0px';
+    } else if(this.className == "bottom animate"){
+      this.style.top = '0px';
+      this.style.left = '0px';
+    } else if(this.className == "protext"){
+      this.style.fontSize = '1.7em';
+      this.style.marginTop = '10px';
+      this.style.marginbottom = '10px';
+      this.style.backgroundColor = 'rgba(255, 218, 218, 0.8)';
+    } else if(this.className == "protextw"){
+      this.style.fontSize = '0.7em';
     }
   })
 }, function() {
   let divChilds = jQuery("div", this);
-  divChilds.each(function(x, elem) {
-    if(elem.className == "top"){
-      elem.style.top = '-220px';
-      elem.style.left = '-20%';
-    } else if(elem.className == "bottom"){
-      elem.style.top = projectHeight;
-      elem.style.left = '10%';
-    } else if(elem.className == "protext"){
-      elem.style.fontSize = '0em';
-      elem.style.marginTop = '50px';
-      elem.style.backgroundColor = 'transparent';
-    } else if(elem.className == "protextw"){
-      elem.style.fontSize = '0em';
+  divChilds.each(function() {
+    if(this.className == "top animate"){
+      this.style.top = '-' + projectHeight + 'px';
+      this.style.left = '-' + projectWidth + 'px';
+    } else if(this.className == "bottom animate"){
+      this.style.top = projectHeight + "px";
+      this.style.left = projectWidth + "px";
+    } else if(this.className == "protext"){
+      this.style.fontSize = '0em';
+      this.style.marginTop = '50px';
+      this.style.backgroundColor = 'transparent';
+    } else if(this.className == "protextw"){
+      this.style.fontSize = '0em';
     }
   })
 });
+
+//------------------------
+// footer Icons hovering
+//------------------------
+
+$('.contacttype').hover(function() {
+  let footerChilds = jQuery("div", this);
+  footerChilds.each(function(elem) {
+  console.log($(this));
+    $(this).find('.contacttype').css({'font-size': '2em'});
+    $(this).find('.footertext').css({'visibility': 'visible', 'color': 'black'});
+  })
+}, function() {
+  let footerChilds = jQuery("div", this);
+  footerChilds.each(function() {
+    $(this).find('.contacttype').css({'font-size': '1em'});
+    $(this).find('.footertext').css({'visibility': 'hidden'});
+  })  
+});
+
