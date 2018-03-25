73e75cd5c0f7def754d13106ced642061e4ea177
Ethan Strauss <estrauss@gmail.com>
2018-03-25 19:30:51 -0400

    Update script.js
    JS less

--- script.js
+++ script.js
@@ -3,69 +3,34 @@
 //------------------------
 let projectWidth = $(".project").width().toString();
 let projectHeight = $(".project").height().toString();
-let bottoms = $('.bottom');
-let tops = $('.top');
 $('.bottom')  
-  .css({ borderBottomWidth: projectHeight + 'px', borderLeftWidth: projectWidth + 'px' })
-  .addClass('animate');
+  .css({ borderBottomWidth: projectHeight + 'px', borderLeftWidth: projectWidth + 'px' });
 $('.top')  
-  .css({ borderRightWidth: projectWidth + 'px', borderTopWidth: projectHeight + 'px' })  
-  .addClass('animate');
+  .css({ borderRightWidth: projectWidth + 'px', borderTopWidth: projectHeight + 'px' });
 
-$('.project').hover(function() {
-  let divChilds = jQuery("div", this);
-  divChilds.each(function() {
-    // console.log(this);
-    if(this.className == "top animate"){
-      this.style.top = '0px';
-      this.style.left = '0px';
-    } else if(this.className == "bottom animate"){
-      this.style.top = '0px';
-      this.style.left = '0px';
-    } else if(this.className == "protext"){
-      this.style.fontSize = '1.7em';
-      this.style.marginTop = '10px';
-      this.style.marginbottom = '10px';
-      this.style.backgroundColor = 'rgba(255, 218, 218, 0.8)';
-    } else if(this.className == "protextw"){
-      this.style.fontSize = '0.7em';
-    }
-  })
-}, function() {
-  let divChilds = jQuery("div", this);
-  divChilds.each(function() {
-    if(this.className == "top animate"){
-      this.style.top = '-' + projectHeight + 'px';
-      this.style.left = '-' + projectWidth + 'px';
-    } else if(this.className == "bottom animate"){
-      this.style.top = projectHeight + "px";
-      this.style.left = projectWidth + "px";
-    } else if(this.className == "protext"){
-      this.style.fontSize = '0em';
-      this.style.marginTop = '50px';
-      this.style.backgroundColor = 'transparent';
-    } else if(this.className == "protextw"){
-      this.style.fontSize = '0em';
-    }
-  })
-});
+//-----------------------
+// Scrolling Position
+//-----------------------
+let historydist = $('#historyslice').offset().top;
+let biodist = $('#bioslice').offset().top;
+let projectdist = $('#projectslice').offset().top;
+let contactdist = $('#contactslice').offset().top;
 
-//------------------------
-// footer Icons hovering
-//------------------------
 
-$('.contacttype').hover(function() {
-  let footerChilds = jQuery("div", this);
-  footerChilds.each(function(elem) {
-  console.log($(this));
-    $(this).find('.contacttype').css({'font-size': '2em'});
-    $(this).find('.footertext').css({'visibility': 'visible', 'color': 'black'});
-  })
-}, function() {
-  let footerChilds = jQuery("div", this);
-  footerChilds.each(function() {
-    $(this).find('.contacttype').css({'font-size': '1em'});
-    $(this).find('.footertext').css({'visibility': 'hidden'});
-  })  
-});
-
+document.addEventListener('scroll', function (event) {
+  if (event.pageY < biodist) {
+    $('a').removeClass();
+  } else if (event.pageY > biodist && event.pageY < historydist) {
+    $('a').removeClass();
+    $('a[href="#bioslice"]').addClass('menunow');
+  } else if (event.pageY > historydist && event.pageY < projectdist) {
+    $('a').removeClass();
+    $('a[href="#historyslice"]').addClass('menunow');
+  } else if (event.pageY > projectdist && event.pageY < contactdist) {
+    $('a').removeClass();
+    $('a[href="#projectslice"]').addClass('menunow');
+  } else if (event.pageY > contactdist && event.pageY < projectdist) {
+    $('a').removeClass();
+    $('a[href="#contactslice"]').addClass('menunow');
+  }
+}, true);
