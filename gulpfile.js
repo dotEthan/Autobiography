67bdffc55079365b435a58f7f79d881c0213a253
Ethan Strauss <estrauss@gmail.com>
2018-09-01 12:50:20 -0400

    switching to VScode

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+const sass = require('gulp-sass');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src(['scss/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest("css"))
-        .pipe(browserSync.stream());
+	return gulp
+		.src(['scss/*.scss'])
+		.pipe(sass())
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.stream());
 });
-
 
 // Watch Sass & Serve
 gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        server: "./"  
-    });
+	browserSync.init({
+		server: './'
+	});
 
-    gulp.watch(['scss/*.scss'], ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
-    gulp.watch("js/*.js").on('change', browserSync.reload);
+	gulp.watch(['scss/*.scss'], ['sass']);
+	gulp.watch('*.html').on('change', browserSync.reload);
+	gulp.watch('js/*.js').on('change', browserSync.reload);
 });
 
 // Default Task
